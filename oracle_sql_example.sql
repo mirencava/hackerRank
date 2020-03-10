9ae21e16f8bfc265396867dfa78f418ec4d69e1c
paiewonsky <35489044+paiewonsky@users.noreply.github.com>
2020-03-10 17:06:39 +0100

    Update oracle_sql_example.sql

--- oracle_sql_example.sql
+++ oracle_sql_example.sql
@@ -9,4 +9,7 @@ select min(city), len
  group by len;
 
                    /*el min del select es porque hay que quedarse con la primera de las apareciones de aquellas agrupaciones que tengan la misma longitud*/
-/*analytic_function([ arguments ]) OVER (analytic_clause)*/
+/*analytic_function([ arguments ]) OVER (analytic_clause)
+                   SELECT empno, deptno, sal, 
+       FIRST_VALUE(sal IGNORE NULLS) OVER (PARTITION BY deptno ORDER BY sal ASC NULLS LAST) AS first_val_in_dept
+FROM   emp;*/
