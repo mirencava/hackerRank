784e236be16fdde850a8aecab0a5907fc305dc7f
miren.cava <miren.cava@gmail.com>
2020-08-24 20:51:01 +0200

    nuevo python

--- python/contruir_puerta.py
+++ python/contruir_puerta.py
@@ -39,8 +39,8 @@ if __name__ == '__main__':
 
 
 N, M = map(int,input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
-for i in range(1,N,2):
-    print('-'*((M-3*i)//2) + '.|.'*i + '-'*((M-3*i)//2)) #Enter Code Here
+for i in range(1,N,2): # 1 3 5
+    print('-'*((M-3*i)//2) + '.|.'*i + '-'*((M-3*i)//2)) #Enter Code Here // division cogiendo el entero
 print('-'*((M-7)//2)+'WELCOME'+'-'*((M-7)//2)) #Enter Code Here
 for i in range(N-2,-1,-2):
     print('-'*((M-3*i)//2) + '.|.'*i + '-'*((M-3*i)//2)) #Enter Code Here
