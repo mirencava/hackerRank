7254413a1fa0338618dd5dbfa07a87bea60518eb
miren.cava <miren.cava@gmail.com>
2020-08-29 14:45:59 +0200

    itertools 2

--- python/operaciones.py
+++ python/operaciones.py
@@ -1,5 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from itertools import product
+
+
 import sys
 if __name__ == '__main__':
     valores= []
@@ -9,3 +11,20 @@ if __name__ == '__main__':
 
 
     [print(item, end = ' ') for item in product(valores[0],valores[1]) ]
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import permutations
+
+if __name__ == '__main__':
+    line = input().split()
+
+    [print(''.join(item)) for item in sorted(permutations(line[0],int(line[1])))]
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import combinations
+if __name__ == '__main__':
+
+    S, k = input().split()
+    for size in range(int(k)):
+        print(*map("".join,combinations(sorted(S), size+1)),sep="\n")
